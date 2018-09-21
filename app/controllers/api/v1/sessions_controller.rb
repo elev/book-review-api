087b6e3d3acdef9939aa29fb28564c6e989d0bad
Lucas Boyd <LucasBoydDev@gmail.com>
2018-09-21 08:30:34 -0400

    Creates login, updates devise

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::SessionsController < Devise::SessionsController
+  before_action :sign_in_params, only: :create
+  before_action :load_user, only: :create
+
+  # sign in
+  def create
+    if @user.valid_password?(sign_in_params[:password])
+      sign_in 'user', @user
+      json_response 'Sign in successful', true, {user: @user}, :ok
+    else
+      json_response 'Unauthorized', false, {}, :unauthorized
+    end
+  end
+
+  private
+
+  def sign_in_params
+    params.require(:sign_in).permit(:email, :password)
+  end
+
+  def load_user
+    @user = User.find_for_database_authentication(email: sign_in_params[:email])
+
+    if @user
+      return @User
+    else
+      json_response 'Cannot find user', false, {}, :failure
+    end
+  end
+end
