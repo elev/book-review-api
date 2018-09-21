fb63f4c5bccc30e92345a034903b1f47ed05f1dd
Lucas Boyd <LucasBoydDev@gmail.com>
2018-09-20 21:45:36 -0400

    Writes test for api v1 registrations controller

---
+++ test/controllers/api/v1/registrations_controller_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+require 'pry'
+class RegistrationsControllerTest < ActionDispatch::IntegrationTest
+  test 'should register user' do
+    post api_v1_sign_up_path(
+      params: { 
+        user: { 
+          email: 'test@foo.com',
+          password: 'foobar',
+          password_confirmation: 'foobar'
+        }
+      }
+    )
+    assert_response :success
+  end
+
+  test 'should throw error with bad password' do
+    post api_v1_sign_up_path(
+      params: { 
+        user: { 
+          email: 'test@foo.com',
+          password: 'foobar',
+          password_confirmation: 'foobardiff'
+        }
+      }
+    )
+    assert_response :unprocessable_entity
+  end
+
+  test 'should throw error with bad params' do
+    post api_v1_sign_up_path(
+      params: {}
+    )
+    assert_response :bad_request
+  end
+end
